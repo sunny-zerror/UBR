5b315ff4aa5cdb8806240049fc1ceefe500b964f
sunny-zerror <sunny.zerror@gmail.com>
2026-05-26 17:35:16 +0530

    animation fix

--- src/components/animation/GlobalImgReveal.jsx
+++ src/components/animation/GlobalImgReveal.jsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation"
 import { useEffect } from "react"
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 gsap.registerPlugin(ScrollTrigger)
 
